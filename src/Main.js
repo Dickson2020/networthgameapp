fd37ff31952455084cb05c0192d41295e1f24f9f
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-28 19:08:45 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,30 +1,56 @@
+import React, { useState } from 'react';
+
 import { DynamicWidget, DynamicEmbeddedWidget, useUserWallets } from "@dynamic-labs/sdk-react-core";
 import './App.css'
 
 const Main = () => {
   const userWallets = useUserWallets()
 
+    const [showNetworth, setShowNetworth] = useState(false);
+  const [showLeaderboard, setShowLeaderboard] = useState(false);
+  const [showHome, setShowHome] = useState(false);
+
+  
+  const handleViewNetworth = () => {
+    setShowNetworth(false);
+        setShowHome(true);
+
+    setShowLeaderboard(false);
+  };
+
+  const handleViewHome = () => {
+    setShowHome(true);
+    setShowHome(false);
+            setShowHome(false);
+
+  };
+
+  const handleViewLeaderboard = () => {
+    setShowNetworth(false);
+            setShowHome(false);
+
+    setShowLeaderboard(true);
+  };
+
   const isConnected = userWallets.some((wallet) => wallet.connected)
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
       <div className="flex flex-col items-center justify-center text-center">
-        <h1 className="text-4xl font-bold mb-4">{isConnected ? 'Hi, welcome' : 'Login'}</h1>
-        {isConnected ? <DynamicEmbeddedWidget /> : <DynamicWidget />}
-        
-        <div>
-          <h1>Connected wallets</h1>
+      {isConnected ? <div className="flex justify-center mb-4">
+         <div className="flex gap-4">
+    
+      <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleViewHome()}>My Wallet</button>
 
-          {userWallets.map((wallet) => (
-            <p key={wallet.id}>
-              {wallet.address}: {wallet.connected ? 'Connected' : 'Not connected'}
-            </p>
-          ))}
-        </div>
-        <div className="flex justify-center mb-4">
-          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Wallet</button>
-          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Leaderboard</button>
-        </div>
+  <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleViewNetworth()}>View Networth</button>
+  <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleViewLeaderboard()}>View Leaderboard</button>
+</div> </div> : '<h4>Networth Game Project</h4>'}
+        {showHome && isConnected ? <DynamicEmbeddedWidget /> : <DynamicWidget />}
+        
+
+      {showNetworth && <NetworthPage />}
+      {showLeaderboard && <LeaderboardPage />}
+        
       </div>
     </div>
   );
