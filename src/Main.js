1f0838e475633510eec4015664a1d3b6a31b193e
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-28 18:43:52 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -11,6 +11,10 @@ const Main = () => {
         <div className="flex flex-col items-center justify-center text-center">
           <h1 className="text-4xl font-bold mb-4">Hi, welcome</h1>
           <DynamicEmbeddedWidget />
+       <div className="flex justify-center mb-4">
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Wallet</button>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Leaderboard</button>
+        </div>
         </div>
       </div>
     );
