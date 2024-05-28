1de35bdf8543a0cfdc23aac2a9593f8cbeb1fe6c
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-28 18:55:59 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,33 @@
-import { DynamicWidget, DynamicEmbeddedWidget,useUserWallets } from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget, DynamicEmbeddedWidget, useUserWallets } from "@dynamic-labs/sdk-react-core";
 import './App.css'
+
 const Main = () => {
   const userWallets = useUserWallets()
 
-  
-    return (
-      <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
-        <div className="flex flex-col items-center justify-center text-center">
-          <h1 className="text-4xl font-bold mb-4">Hi, welcome</h1>
-          <DynamicEmbeddedWidget />
+  const isConnected = userWallets.some((wallet) => wallet.connected)
 
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-4xl font-bold mb-4">{isConnected ? 'Hi, welcome' : 'Login'}</h1>
+        {isConnected ? <DynamicEmbeddedWidget /> : <DynamicWidget />}
+        
         <div>
-      <h1>Connected wallets</h1>
+          <h1>Connected wallets</h1>
 
-      {userWallets.map((wallet) => (
-        <p key={wallet.id}>
-          {wallet.address}: {wallet.connected ? 'Connected' : 'Not connected'}
-        </p>
-      ))}
-    </div>
-       <div className="flex justify-center mb-4">
+          {userWallets.map((wallet) => (
+            <p key={wallet.id}>
+              {wallet.address}: {wallet.connected ? 'Connected' : 'Not connected'}
+            </p>
+          ))}
+        </div>
+        <div className="flex justify-center mb-4">
           <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Wallet</button>
           <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">Leaderboard</button>
         </div>
-        </div>
       </div>
-    );
-  
+    </div>
+  );
 };
 
 export default Main;
