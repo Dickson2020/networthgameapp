7a079cda4968442438e4ef567b19ca291a2cda33
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-28 18:47:12 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,8 @@
-import { DynamicWidget, DynamicEmbeddedWidget, useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import { DynamicWidget, DynamicEmbeddedWidget } from "@dynamic-labs/sdk-react-core";
 import './App.css'
 const Main = () => {
-  const dynamicContext = useDynamicContext();
 
-  if (dynamicContext?.wallet?.connected) {
-    // Fetch token balances for the connected account
-
+  
     return (
       <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
         <div className="flex flex-col items-center justify-center text-center">
@@ -18,16 +15,7 @@ const Main = () => {
         </div>
       </div>
     );
-  } else {
-    return (
-      <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex flex-col items-center justify-center text-white">
-        <div className="flex flex-col items-center justify-center text-center">
-          <h1 className="text-4xl font-bold mb-4">Networth + Web3 by Dickson</h1>
-          <DynamicWidget />
-        </div>
-      </div>
-    );
-  }
+  
 };
 
 export default Main;
