5013da3dc705738476678cd3194d57bbe2c6b32a
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-30 21:11:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
 import Main from "./Main";
 
-const App = (({ checkUserExists }) ) => (
+const App = () => (
   <DynamicContextProvider
     settings={{
       environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
