ebf02090eb178d80d893aa5911fa79fddf82d933
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-30 21:53:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { useState } from 'react';
 
-
 import Main from "./Main";
 
-const App = () => (
+const App = () => {
+  const [isFirstTime, setIsFirstTime] = useState(false);
 
-    const [isFirstTime, setIsFirstTime] = useState(false);
-
-
-  <DynamicContextProvider
-    settings={{
-      environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
-      walletConnectors: [EthereumWalletConnectors],
-      events: {
-      onAuthSuccess: async (args) => {
-        const userId = args.primaryWallet.address;
-         alert(userId);
-        setIsFirstTime(true);
-      }
-    }
-    }}
-  >
-    <Main  isFirstTime={isFirstTime}  />
-  </DynamicContextProvider>
-);
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
+        walletConnectors: [EthereumWalletConnectors],
+        events: {
+          onAuthSuccess: async (args) => {
+            const userId = args.primaryWallet.address;
+            alert(userId);
+            setIsFirstTime(true);
+          }
+        }
+      }}
+    >
+      <Main isFirstTime={isFirstTime} />
+    </DynamicContextProvider>
+  );
+};
 
 export default App;
