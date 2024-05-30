78c84a7d2d19922b5494c1a1eb8ed7ef909cdb65
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-30 20:59:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const App = () => (
     settings={{
       environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
       walletConnectors: [EthereumWalletConnectors],
+      events: {
+      onAuthSuccess: async (args) => {
+        const userId = args.primaryWallet.address;
+         alert(userId);
+      }
+    }
     }}
   >
     <Main />
