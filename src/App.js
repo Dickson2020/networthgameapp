78b79e4079bb23fb827cef696ffe2b61029c9ef9
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-05-31 11:24:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { useState } from 'react';
+import React, { useState, useState, useEffect } from 'react';
+import supabase from './supabase';
 
 import Main from "./Main";
 
 const App = () => {
   const [isFirstTime, setIsFirstTime] = useState(false);
 
+  const checkUserExists = async (userId) => {
+    const { data, error } = await supabase
+      .from('networth') // Replace with your actual table name
+      .select()
+      .eq('user_id', userId)
+      .single();
+
+    if (error && userId.toString().length > 1) {
+      const newUser = { user_id: userId, counter: 1 }; // Replace with the actual user data
+      const { error: insertError } = await supabase
+        .from('networth')
+        .insert(newUser);
+
+      if (insertError) {
+        console.error("Record error:", insertError);
+      } else {
+     alert(`User with ID ${userId} recorded in Supabase`);
+      }
+    } else if (data) {
+        
+
+      const currentCounterValue = parseInt(data.counter);
+     // alert(data.counter)
+       
+      const updatedCounterValue = currentCounterValue + 1;
+      const { error: updateError } = await supabase
+        .from('networth')
+        .update({ counter: updatedCounterValue })
+        .eq('id', data.id);
+
+      if (updateError) {
+        alert("Update counter error:", updateError);
+      } else {
+      alert("Networth updated successfully:", userId);
+       // updateNetworthValue(updatedCounterValue);
+      }
+       
+    
+    
+    }
+  }
   return (
     <DynamicContextProvider
       settings={{
@@ -15,8 +57,8 @@ const App = () => {
         events: {
           onAuthSuccess: async (args) => {
             const userId = args.primaryWallet.address;
-            alert(userId);
-            setIsFirstTime(true);
+            checkUserExists(userId);
+          //  setIsFirstTime(true);
           }
         }
       }}
