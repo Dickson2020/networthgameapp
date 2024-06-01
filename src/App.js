62b830c53fe1b4de8cc20da3656ab5598414af34
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-06-01 16:00:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,41 +8,39 @@ import Main from "./Main";
 const App = () => {
   const [isFirstTime, setIsFirstTime] = useState(false);
 
-
   return (
     <DynamicContextProvider
       settings={{
         environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
         walletConnectors: [EthereumWalletConnectors],
-       events: {
-  onAuthSuccess: (args) => {
-    const userId = args.primaryWallet.address;
+        events: {
+          onAuthSuccess: (args) => {
+            const userId = args.primaryWallet.address;
 
-    fetch(`https://backend-rose-xi.vercel.app/getuser?user_id=${userId}`)
-      .then(response => response.json())
-      .then(userData => {
-        if (userData) {
-          // User exists, update counter
-          const currentCounterValue = parseInt(userData.counter);
-          const updatedCounterValue = currentCounterValue + 1;
+            fetch(`https://backend-rose-xi.vercel.app/getuser?user_id=${userId}`)
+              .then(response => response.json())
+              .then(userData => {
+                if (userData) {
+                  // User exists, update counter
+                  const currentCounterValue = parseInt(userData.counter);
+                  const updatedCounterValue = currentCounterValue + 1;
 
-          fetch(`https://backend-rose-xi.vercel.app/updateuser?user_id=${userId}&counter=${updatedCounterValue}`)
-            .then(response => response.json())
-            .then(data => alert(JSON.stringify(data)))
-            .catch(error => alert(JSON.stringify(error)));
-        } else {
-          // User doesn't exist, create new user
-          const counter = 1;
-          fetch(`https://backend-rose-xi.vercel.app/createuser?user_id=${userId}&counter=${counter}`)
-            .then(response => response.json())
-            .then(data => alert(JSON.stringify(data)))
-            .catch(error => alert(JSON.stringify(error)));
-        }
-      })
-      .catch(error => alert(JSON.stringify(error)));
-  }
-}
-        }
+                  fetch(`https://backend-rose-xi.vercel.app/updateuser?user_id=${userId}&counter=${updatedCounterValue}`)
+                    .then(response => response.json())
+                    .then(data => alert(JSON.stringify(data)))
+                    .catch(error => alert(JSON.stringify(error)));
+                } else {
+                  // User doesn't exist, create new user
+                  const counter = 1;
+                  fetch(`https://backend-rose-xi.vercel.app/createuser?user_id=${userId}&counter=${counter}`)
+                    .then(response => response.json())
+                    .then(data => alert(JSON.stringify(data)))
+                    .catch(error => alert(JSON.stringify(error)));
+                }
+              })
+              .catch(error => alert(JSON.stringify(error)));
+          }
+        } // <--- Removed extra closing brace
       }}
     >
       <Main isFirstTime={isFirstTime} />
@@ -51,4 +49,3 @@ const App = () => {
 };
 
 export default App;
-      
