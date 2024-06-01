ed517a6bb422f0732224758490b0cce708e05eee
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-06-01 13:09:09 +0100

    Update LeaderboardScore.js

--- src/LeaderboardScore.js
+++ src/LeaderboardScore.js
@@ -1,20 +1,13 @@
 
 import React from 'react';
 
-const LeaderboardScore = ({ rank, address, netWorth, change24h, tokens }) => {
+const LeaderboardScore = ({ rank, address, netWorth }) => {
   return (
     <div className="leaderboard-score">
       <div className="rank">{rank}</div>
       <div className="address">{address}</div>
       <div className="net-worth">{netWorth} Net worth</div>
-      <div className="tokens">
-        {tokens.map((token, index) => (
-          <div key={index}>
-            <span>{token.symbol}</span>
-            <span>{token.balance}</span>
-          </div>
-        ))}
-      </div>
+      
     </div>
   );
 };
