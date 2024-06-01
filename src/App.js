104bfdc1ad2006533e3c690c07c26d094b497bce
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-06-01 15:52:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,43 +8,40 @@ import Main from "./Main";
 const App = () => {
   const [isFirstTime, setIsFirstTime] = useState(false);
 
-const checkUserExists = async (userId) => {
-  //Prism ORM Supabase endpoint
-  const response = await fetch(`https://backend-rose-xi.vercel.app/getuser?user_id=${userId}`);
-  const userData = await response.json();
 
-  if (userData) {
-    // User exists, update counter
-    const currentCounterValue = parseInt(userData.counter);
-    const updatedCounterValue = currentCounterValue + 1;
-
-    fetch(`https://backend-rose-xi.vercel.app/updateuser?user_id=${userId}&counter=${updatedCounterValue}`)
-      .then(response => response.json())
-      .then(data => alert(JSON.stringify(data)))
-      .catch(error => alert(JSON.stringify(error)));
-    
-  } else {
-    
-    // User doesn't exist, create new user
-    const counter = 1;
-
-    fetch(`https://backend-rose-xi.vercel.app/createuser?user_id=${userId}&counter=${counter}`)
-      .then(response => response.json())
-      .then(data => alert(JSON.stringify(data)))
-      .catch(error => alert(JSON.stringify(error)));
-  }
-};
-  
   return (
     <DynamicContextProvider
       settings={{
         environmentId: "c879278a-d3e2-4295-a59e-3ecf5a9695d3",
         walletConnectors: [EthereumWalletConnectors],
-        events: {
-          onAuthSuccess: async (args) => {
-            const userId = args.primaryWallet.address;
-            await checkUserExists(userId);
-          }
+       events: {
+  onAuthSuccess: (args) => {
+    const userId = args.primaryWallet.address;
+
+    fetch(`https://backend-rose-xi.vercel.app/getuser?user_id=${userId}`)
+      .then(response => response.json())
+      .then(userData => {
+        if (userData) {
+          // User exists, update counter
+          const currentCounterValue = parseInt(userData.counter);
+          const updatedCounterValue = currentCounterValue + 1;
+
+          fetch(`https://backend-rose-xi.vercel.app/updateuser?user_id=${userId}&counter=${updatedCounterValue}`)
+            .then(response => response.json())
+            .then(data => alert(JSON.stringify(data)))
+            .catch(error => alert(JSON.stringify(error)));
+        } else {
+          // User doesn't exist, create new user
+          const counter = 1;
+          fetch(`https://backend-rose-xi.vercel.app/createuser?user_id=${userId}&counter=${counter}`)
+            .then(response => response.json())
+            .then(data => alert(JSON.stringify(data)))
+            .catch(error => alert(JSON.stringify(error)));
+        }
+      })
+      .catch(error => alert(JSON.stringify(error)));
+  }
+}
         }
       }}
     >
