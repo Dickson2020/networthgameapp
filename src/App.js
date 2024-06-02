15577d4f1be1133d522411b4af5c706f66b83af7
Miracle Adah <75673864+Dickson2020@users.noreply.github.com>
2024-06-02 22:04:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { createConfig, WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "viem";
+import { mainnet } from "viem/chains";
+import { DynamicEmbeddedWidget, useUserWallets } from "@dynamic-labs/sdk-react-core";
+import { useIsLoggedIn } from '@dynamic-labs/sdk-react-core';
+
+
+const config = createConfig({
+  chains: [mainnet],
+  multiInjectedProviderDiscovery: false,
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
     <DynamicContextProvider
@@ -11,7 +33,7 @@ export default function App() {
           <DynamicWagmiConnector>
             <div>
               <DynamicEmbeddedWidget />
-              <MyComponentWrapper /> {/* Wrap MyComponent with MyComponentWrapper */}
+              <MyComponentWrapper />
             </div>
           </DynamicWagmiConnector>
         </QueryClientProvider>
